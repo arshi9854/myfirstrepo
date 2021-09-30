17acaf784cc093ec44498061482481c1e2073acd
arshi9854 <shaikarshiya463@gmail.com>
2021-09-30 18:07:37 +0530

    Main updated

---
+++ src/secondgit/Main.java
@@ -0,0 +1,11 @@
+package secondgit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to the github");
+		// TODO Auto-generated method stub
+
+	}					
+
+}
